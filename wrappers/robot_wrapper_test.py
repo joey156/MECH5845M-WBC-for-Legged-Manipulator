80cb1fbe4df6f843836f3e0c3c821eedcd2f16b3
joey156 <75869222+joey156@users.noreply.github.com>
2021-03-31 13:11:56 +0100

    Moving test file due to stack smashing.

---
+++ wrappers/robot_wrapper_test.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.append("/home/joey156/Disso_ws/MECH5845M-WBC-for-Legged-Manipulator/wrappers/")
+from Robot_Wrapper import RobotModel
+#from scipy.spatial.transform import Rotation as R
+import numpy as np
+
+
+urdf = "/home/joey156/Disso_ws/MECH5845M-WBC-for-Legged-Manipulator/Robot_Descriptions/urdf/a1_wx200.urdf"
+
+LeggedRobot = RobotModel(urdf)
+
+LeggedRobot.neutralConfig()
+
+
+r = LeggedRobot.EndEffectorJacobians()
+lower_pos_lim, upper_pos_lim = LeggedRobot.jointPosLimitsArray()
+lower_vel_lim, upper_vel_lim = LeggedRobot.jointVelLimitsArray()
+print(lower_pos_lim)
+print("\n", lower_pos_lim.shape)
+x = LeggedRobot.robot_data.oMi[19].rotation
+x = np.array(x)
+print(x)
+
+x = LeggedRobot.Rot2Euler(x)
+com_target_pos = [1, 1, 1]
+com_target_vel = [1, 1, 1]
+EE_pos = np.array([[1,1,1]]).T
+EE_vel = np.array([[1,1,1,1,1,1]]).T
+print(EE_pos)
+EE_target_pos = [EE_pos, EE_pos, EE_pos, EE_pos, EE_pos]
+EE_target_vel = [EE_vel, EE_vel, EE_vel, EE_vel, EE_vel]
+LeggedRobot.cartesianTargetsEE(EE_target_pos, EE_target_vel)
+print(LeggedRobot.cartesian_targetsEE)
+LeggedRobot.cartesianTargetCoM(com_target_pos, com_target_vel)
+print(LeggedRobot.cartesian_targetsCoM.shape)
+print("success")
+print(LeggedRobot.robot_data.oMi[19].translation.shape)
+
