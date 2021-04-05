ef8e4605065abe98c82137c2e5829d28b2e8dbf7
joey156 <75869222+joey156@users.noreply.github.com>
2021-04-05 01:04:05 +0100

    Test files have been updated.

--- wrappers/QPtest1.py
+++ wrappers/QPtest1.py
@@ -1,6 +1,10 @@
 import numpy as np
 import qpsolvers as qp
 from Robot_Wrapper import RobotModel
+from qpoases import PyQProblemB as QProblemB
+from qpoases import PyBooleanType as BooleanType
+from qpoases import PySubjectToStatus as SubjectToStatus
+from qpoases import PyOptions as Options
 
 urdf = "/home/joey156/Disso_ws/MECH5845M-WBC-for-Legged-Manipulator/Robot_Descriptions/urdf/a1_wx200.urdf"
 
@@ -9,12 +13,47 @@ LeggedRobot = RobotModel(urdf)
 LeggedRobot.neutralConfig()
 
 #targets
-com_target_pos = np.array([[1, 1, 1]]).T
+com_target_pos = np.array([[2, 2, 2]]).T
 
 planner_pos, planner_vel = LeggedRobot.posAndVelTargetsCoM(com_target_pos)
+
 
 lower_vel_lim, upper_vel_lim = LeggedRobot.jointVelLimitsArray()
 lower_pos_lim, upper_pos_lim = LeggedRobot.jointPosLimitsArray()
 
-lb = np.concatenate((lower_vel_lim.T, lower_pos_lim), axis=0)
-ub = np.concatenate((upper_vel_lim.T, upper_pos_lim), axis=0)
+lb = np.concatenate((lower_vel_lim.T, lower_pos_lim), axis=0).reshape((52,))
+#lb = lower_vel_lim.reshape(26,)
+#print(lb.shape)
+
+ub = np.concatenate((upper_vel_lim.T, upper_pos_lim), axis=0).reshape((52,))
+#ub = upper_vel_lim.reshape(26,)
+#print(ub)
+
+print(lb)
+A = np.dot(LeggedRobot.comJ.T, LeggedRobot.comJ)
+#print(LeggedRobot.comJ)
+#print(A)
+b = LeggedRobot.qpCartesianB(planner_pos[499], planner_vel[499]).reshape((3,))
+#print(b)
+b = np.dot(b.T, LeggedRobot.comJ)
+#print(b)
+print(b.shape)
+
+qp = QProblemB(26)
+
+options = Options()
+options.enableFlippingBounds = BooleanType.FALSE
+#options.initialStatusBounds = SubjectToStatus.INACTIVE
+options.numRefinementSteps = 10
+
+qp.setOptions(options)
+
+nWSR = np.array([100000])
+qp.init(A, b, lb, ub, nWSR)
+
+#print("\nnWSR = %d\n\n"%nWSR)
+
+xOpt = np.zeros((26,))
+qp.getPrimalSolution(xOpt)
+print("\nxOpt = [ %e, %e ]; objCal = %e\n\n" %(xOpt[0], xOpt[1], qp.getObjVal()))
+print(xOpt)
--- wrappers/robot_wrapper_test.py
+++ wrappers/robot_wrapper_test.py
@@ -3,37 +3,44 @@ sys.path.append("/home/joey156/Disso_ws/MECH5845M-WBC-for-Legged-Manipulator/wra
 from Robot_Wrapper import RobotModel
 #from scipy.spatial.transform import Rotation as R
 import numpy as np
+import pinocchio as pin
 
 
 urdf = "/home/joey156/Disso_ws/MECH5845M-WBC-for-Legged-Manipulator/Robot_Descriptions/urdf/a1_wx200.urdf"
 
 LeggedRobot = RobotModel(urdf)
 
-LeggedRobot.neutralConfig()
+#LeggedRobot.neutralConfig()
 
 
 r = LeggedRobot.EndEffectorJacobians()
 lower_pos_lim, upper_pos_lim = LeggedRobot.jointPosLimitsArray()
 lower_vel_lim, upper_vel_lim = LeggedRobot.jointVelLimitsArray()
-print(lower_pos_lim)
-print("\n", lower_pos_lim.shape)
+#print(lower_pos_lim)
+#print("\n", lower_pos_lim.shape)
 x = LeggedRobot.robot_data.oMi[19].rotation
 x = np.array(x)
-print(x)
-
+#print(x)
+#print(LeggedRobot.current_joint_config)
 x = LeggedRobot.Rot2Euler(x)
 com_target_pos = np.array([[1, 1, 1]]).T
 com_target_vel = np.array([[1, 1, 1]]).T
 EE_pos = np.array([[1,1,1]]).T
-EE_vel = np.array([[1,1,1,1,1,1]]).T
-print(EE_pos)
+EE_vel = np.array([[1,1,1,0,0,0]]).T
+#print(EE_pos)
 EE_target_pos = [EE_pos, EE_pos, EE_pos, EE_pos, EE_pos]
 EE_target_vel = [EE_vel, EE_vel, EE_vel, EE_vel, EE_vel]
 LeggedRobot.cartesianTargetsEE(EE_target_pos, EE_target_vel)
-print(LeggedRobot.cartesian_targetsEE)
+#print(LeggedRobot.cartesian_targetsEE)
 LeggedRobot.cartesianTargetCoM(com_target_pos, com_target_vel)
-print(LeggedRobot.cartesian_targetsCoM)
-print("success")
-print(LeggedRobot.robot_data.oMi[19].translation.shape)
+#print(LeggedRobot.cartesian_targetsCoM)
+#print("success")
+#print(LeggedRobot.robot_data.oMi[19].translation.shape)
 planner_pos, planner_vel = LeggedRobot.posAndVelTargetsCoM(com_target_pos)
-print(planner_vel[0])
+#print(planner_vel[0])
+#LeggedRobot.printJointCart()
+print(LeggedRobot.robot_model.getFrameId("FL_foot_fixed", pin.FIXED_JOINT))
+print(LeggedRobot.robot_model.getFrameId("FR_foot_fixed", pin.FIXED_JOINT))
+print(LeggedRobot.robot_model.getFrameId("RL_foot_fixed", pin.FIXED_JOINT))
+print(LeggedRobot.robot_model.getFrameId("RR_foot_fixed", pin.FIXED_JOINT))
+print(LeggedRobot.robot_model.getFrameId("gripper_bar", pin.FIXED_JOINT))
