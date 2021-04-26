2e211902320602030ceaf3087d6ff6c0587748ae
joey156 <75869222+joey156@users.noreply.github.com>
2021-04-26 03:55:23 +0100

    Testing the klampt library.
    The klampt library will be used to generate the tragectories of the legged manipulator. This file is used to test this library.

---
+++ tests_NOT_FOR_USE/klampt_test.py
@@ -0,0 +1,22 @@
+from klampt.model import trajectory
+from klampt import vis
+import numpy as np
+
+milestones = [[0.252,0,0.207],[0.35,0,0.207],[0.5,0,0],[0.5,0,-0.15]]
+
+traj = trajectory.Trajectory(milestones=milestones)
+
+
+vis.add("point",[0,0,0])
+vis.animate("point",traj)
+vis.add("traj",traj)
+vis.spin(float('inf'))   #show the window until you close it
+
+traj2 = trajectory.HermiteTrajectory()
+traj2.makeSpline(traj)
+
+vis.animate("point",traj2)
+vis.add("traj2",traj2)
+vis.spin(float('inf'))
+
+print(np.array(traj2.eval(0.1)).shape)
