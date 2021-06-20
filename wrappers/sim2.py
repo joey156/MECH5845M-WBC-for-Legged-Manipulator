e5c980eccbda98773f2fd4b98422b8b5cc9967b6
joey156 <75869222+joey156@users.noreply.github.com>
2021-06-20 04:39:47 +0100

    Added chickenhead simulation.
    A new simulation has been added. In this simulation, the robot arm gripper tries to stay in a constant location while the trunk moves in space. Currently, only A1-wx200 can complete this sim.

--- wrappers/sim2.py
+++ wrappers/sim2.py
@@ -3,12 +3,12 @@ import time
 import pinocchio as pin
 import numpy as np
 from QP_Wrapper import QP
-from Robot_Wrapper import RobotModel
+from Robot_Wrapper2 import RobotModel
 from klampt.model import trajectory
 
 # define robot to use: "a1_wx200", "a1_px100", "laikago_vx300"
-#robot = "a1_wx200"
-robot = "a1_px100"
+robot = "a1_wx200"
+#robot = "a1_px100"
 #robot = "laikago_vx300"
 if robot == "a1_wx200":
     robot_index = 0
@@ -19,6 +19,14 @@ if robot == "a1_px100":
 if robot == "laikago_vx300":
     robot_index = 2
     foot_offset = False
+
+# select simulation
+#sim = "static reach"
+sim = "chickenhead"
+if sim == "static reach":
+    sim_id = 0
+if sim == "chickenhead":
+    sim_id = 1
 
 # setup simulation parameters
 p.connect(p.GUI)
@@ -46,14 +54,15 @@ print(LeggedRobot.current_joint_config)
 # load the legged robot urdf
 LeggedRobot_bullet = p.loadURDF(urdf_path_list[robot_index],[0,0,1],[0,0,0,1], flags=urdfFlags, useFixedBase=False)
 
+#LeggedRobot.printJointCart()
 
 # Removing the collision pairs between the two gripper fingers and the bar they are attached to
-if robot_index == 0:
+if robot_index == 0 or robot_index == 2:
     p.setCollisionFilterPair(LeggedRobot_bullet, LeggedRobot_bullet, 28, 29, 0)
     p.setCollisionFilterPair(LeggedRobot_bullet, LeggedRobot_bullet, 26, 28, 0)
     p.setCollisionFilterPair(LeggedRobot_bullet, LeggedRobot_bullet, 26, 29, 0)
 
-if robot_index == 1 or robot_index == 2:
+if robot_index == 1:
     p.setCollisionFilterPair(LeggedRobot_bullet, LeggedRobot_bullet, 27, 28, 0)
     p.setCollisionFilterPair(LeggedRobot_bullet, LeggedRobot_bullet, 25, 27, 0)
     p.setCollisionFilterPair(LeggedRobot_bullet, LeggedRobot_bullet, 25, 28, 0)
@@ -87,7 +96,8 @@ for i in range(len(EE_ID_py)):
             EE_ID_py[i] = j
 
 # set intitail simulation configuration
-joints_py = LeggedRobot.current_joint_config[7:]
+#joints_py = LeggedRobot.current_joint_config[7:]
+joints_py = np.hstack((LeggedRobot.FR_leg, LeggedRobot.FL_leg, LeggedRobot.RR_leg, LeggedRobot.RL_leg, LeggedRobot.grip))
 maxForce = p.readUserDebugParameter(maxForceId)
 
 for i in range(len(joints_py)):
@@ -101,7 +111,7 @@ while (time.time()- t) < 2:
 
 print("Y", abs(LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[0]].translation[2]))
 
-p.resetBasePositionAndOrientation(LeggedRobot_bullet, [0, 0, (abs(LeggedRobot.robot_data.oMf[LeggedRobot.trunk_frame_index].translation[2]))], [0, 0, 0, 1])
+p.resetBasePositionAndOrientation(LeggedRobot_bullet, [0, 0, (abs(LeggedRobot.robot_data.oMf[LeggedRobot.trunk_frame_index].translation[2]))+0.01], [0, 0, 0, 1])
 
 # allow simulation to settle
 t = time.time()
@@ -111,18 +121,29 @@ while (time.time()- t) < 2:
 # Simulation camera settings
 p.getCameraImage(1000,1000)
 #p.resetDebugVisualizerCamera(1.0,1.25,-19.8,[0.07,0.1,0.07])
-p.resetDebugVisualizerCamera(1.00,63.65,-31.4,[0.04,0.03,0.13])
+if robot_index == 0 or robot_index == 1:
+    p.resetDebugVisualizerCamera(1.00,63.65,-31.4,[0.04,0.03,0.13])
+if robot_index == 2:
+    p.resetDebugVisualizerCamera(1.20,63.65,-31.4,[0.04,0.03,0.13])
 p.setRealTimeSimulation(1)
 
 # select the tasks that are active
 LeggedRobot.setTasks(EE=True, CoM=False, Trunk=True, Joint="MANI")
 
-# setting objectives
+# setting objectives#
+"""
 EE_pos_FR = np.array([LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[0]].translation]).T #np.array([[0.174, -0.142, -0.32]]).T
 EE_pos_FL = np.array([LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[1]].translation]).T #np.array([[0.163, 0.144, -0.32]]).T
 EE_pos_RR = np.array([LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[2]].translation]).T #np.array([[-0.196, -0.148, -0.32]]).T
 EE_pos_RL = np.array([LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[3]].translation]).T #np.array([[-0.203, 0.148, -0.32]]).T
 EE_pos_GRIP = np.array([LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[4]].translation]).T #np.array([[0.202, 0., 0.227]]).T #base: 0.252 0. 0.207 reach 0.5 0 -0.15
+"""
+EE_pos_FR = np.array([p.getLinkState(LeggedRobot_bullet, EE_ID_py[0])[4]]).T
+EE_pos_FL = np.array([p.getLinkState(LeggedRobot_bullet, EE_ID_py[1])[4]]).T
+EE_pos_RR = np.array([p.getLinkState(LeggedRobot_bullet, EE_ID_py[2])[4]]).T
+EE_pos_RL = np.array([p.getLinkState(LeggedRobot_bullet, EE_ID_py[3])[4]]).T
+EE_pos_GRIP = np.array([p.getLinkState(LeggedRobot_bullet, EE_ID_py[4])[4]]).T
+
 EE_vel = np.array([[0,0,0,0,0,0]]).T
 Trunk_target_pos = np.array([LeggedRobot.robot_data.oMf[LeggedRobot.trunk_frame_index].translation]).T # 0.013 0.002 0.001
 Trunk_target_vel = np.array([[0,0,0,0,0,0]]).T
@@ -156,19 +177,40 @@ while (1):
 
     planner_pos, planner_vel = LeggedRobot.posAndVelTargetsCoM(com_target_pos)
 
-    # setting gripper trajectory
-    current_gripper_pos = LeggedRobot.robot_data.oMf[53].translation
-    print(current_gripper_pos)
+    
+
+    # set weight mode
+    if sim_id == 0:
+        LeggedRobot.staticReachMode()
+    if sim_id == 1:
+        LeggedRobot.chickenheadMode()
+
+    # finding current states
+    current_gripper_pos = np.array([p.getLinkState(LeggedRobot_bullet, EE_ID_py[4])[4]]).reshape(3,)
+    current_trunk_pos = np.array([p.getLinkState(LeggedRobot_bullet, 1)[4]]).reshape(3,)
+    
     # defining trajectories
+    a1_wx200_trunk_milestones = [current_trunk_pos.tolist(), [0, 0, 0.15], [0, 0, 0.48], current_trunk_pos.tolist(), [0, -0.15, current_trunk_pos.tolist()[2]], [0, 0.15, current_trunk_pos.tolist()[2]], current_trunk_pos.tolist(), [-0.1, 0, current_trunk_pos.tolist()[2]], [0.08, 0, current_trunk_pos.tolist()[2]], current_trunk_pos.tolist()]
     a1_wx200_milestones = [current_gripper_pos.tolist(), [0.4, 0., 0.58], [0.4, -0.2, 0.58], [0.3, -0.35, 0.58], [0, -0.35, 0.58], [-0.1, -0.35, 0.48], [-0.1, -0.35, 0.43], [0, -0.35, 0.29], [0.3, -0.35, 0.29], [0.4, -0.2, 0.29], [0.4, 0.2, 0.29], [0.3, 0.35, 0.29], [0, 0.35, 0.29], [-0.1, 0.35, 0.43], [-0.1, 0.35, 0.48], [0, 0.35, 0.58], [0.3, 0.35, 0.58], [0.4, 0.2, 0.58], [0.4, 0, 0.58]]
-    a1_px100_milestones = [current_gripper_pos.tolist(), [0.28, 0., 0.55], [0.28, -0.1, 0.55], [0.22, -0.22, 0.55], [0.07, -0.22, 0.55], [0., -0.22, 0.51], [0., -0.22, 0.47], [0.07, -0.22, 0.44], [0.22, -0.22, 0.44],[0.28, -0.1, 0.44], [0.28, 0.1, 0.44], [0.22, 0.22, 0.44], [0.07, 0.22, 0.44], [0, 0.22, 0.47], [0, 0.22, 0.51], [0.07, 0.22, 0.55], [0.22, 0.22, 0.55], [0.28, 0.1, 0.55], [0.28, 0, 0.55]]
+    a1_px100_milestones = [current_gripper_pos.tolist(), [0.28, 0., 0.55], [0.28, -0.1, 0.55], [0.22, -0.2, 0.55], [0.07, -0.2, 0.55], [0., -0.2, 0.52], [0., -0.2, 0.47], [0.07, -0.2, 0.44], [0.22, -0.20, 0.44],[0.32, -0.05, 0.44], [0.32, 0.1, 0.44], [0.22, 0.2, 0.44], [0.07, 0.2, 0.44], [0, 0.2, 0.47], [0, 0.2, 0.52], [0.07, 0.2, 0.55], [0.22, 0.2, 0.55], [0.28, 0.1, 0.55], [0.28, 0, 0.55]]
     laikago_vx300_milestones = [current_gripper_pos.tolist(), [0.4, 0., 0.80], [0.4, -0.2, 0.80], [0.3, -0.35, 0.80], [0, -0.35, 0.80], [-0.1, -0.35, 0.75], [-0.1, -0.35, 0.70], [0, -0.35, 0.59], [0.3, -0.35, 0.59], [0.4, -0.2, 0.59], [0.4, 0.2, 0.59], [0.3, 0.35, 0.59], [0, 0.35, 0.59], [-0.1, 0.35, 0.70], [-0.1, 0.35, 0.75], [0, 0.35, 0.80], [0.3, 0.35, 0.80], [0.4, 0.2, 0.80], [0.4, 0, 0.80]]
-    milestones_list = [a1_wx200_milestones, a1_px100_milestones, laikago_vx300_milestones]
+
+    # select trajectory milestones
+    if sim_id == 0:
+        milestones_list = [a1_wx200_milestones, a1_px100_milestones, laikago_vx300_milestones]
+    if sim_id == 1:
+        milestones_list = [a1_wx200_trunk_milestones]
     milestones = milestones_list[robot_index]
+
+    # create trajectory
     traj = trajectory.Trajectory(milestones=milestones)
     traj2 = trajectory.HermiteTrajectory()
-    traj2.makeSpline(traj)  
-    traj_interval = np.arange(0,len(milestones),0.002).tolist()
+    traj2.makeSpline(traj)
+    if sim_id == 0:
+        interval = 0.002
+    if sim_id == 1:
+        interval = 0.001
+    traj_interval = np.arange(0,len(milestones),interval).tolist()
 
     # plot desired trajectory
     previouse_traj_point = np.add(traj2.eval(0), 0).tolist()
@@ -178,6 +220,33 @@ while (1):
         previouse_traj_point = current_traj_point
 
     print(LeggedRobot.com_weight)
+
+    print("CoM:")
+    print(LeggedRobot.robot_data.com[0])
+    print("Trunk:")
+    print("pin:", LeggedRobot.robot_data.oMf[LeggedRobot.trunk_frame_index].translation)
+    print("bullet:", p.getLinkState(LeggedRobot_bullet, 1)[4])
+    print("\n")
+    print("FR:")
+    print("pin:", LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[0]].translation)
+    print("bullet:", p.getLinkState(LeggedRobot_bullet, EE_ID_py[0])[4])
+    print("\n")
+    print("FL:")
+    print("pin:", LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[1]].translation)
+    print("bullet:", p.getLinkState(LeggedRobot_bullet, EE_ID_py[1])[4])
+    print("\n")
+    print("RR:")
+    print("pin:", LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[2]].translation)
+    print("bullet:", p.getLinkState(LeggedRobot_bullet, EE_ID_py[2])[4])
+    print("\n")
+    print("RL:")
+    print("pin:", LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[3]].translation)
+    print("bullet:", p.getLinkState(LeggedRobot_bullet, EE_ID_py[3])[4])
+    print("\n")
+    print("Grip:")
+    print("pin:", LeggedRobot.robot_data.oMf[LeggedRobot.end_effector_index_list_frame[4]].translation)
+    print("bullet:", p.getLinkState(LeggedRobot_bullet, EE_ID_py[4])[4])
+    print(joints_py)
     
     t = time.time()
     while (time.time()- t) < 2:
@@ -202,9 +271,12 @@ while (1):
         
         maxForce = p.readUserDebugParameter(maxForceId)
 
-        # Find new gripper position
-        EE_pos_GRIP = np.array([traj2.eval(i)]).T
-        EE_target_pos[4] = EE_pos_GRIP
+        # Find new position of frame to move
+        if sim_id == 0:
+            EE_pos_GRIP = np.array([traj2.eval(i)]).T
+            EE_target_pos[4] = EE_pos_GRIP
+        if sim_id == 1:
+            Trunk_target_pos = np.array([traj2.eval(i)]).T
 
         # fetch the IMU data for potision and orientation in the world frame
         imu_state = np.array(p.getLinkState(LeggedRobot_bullet, 1)[3])
@@ -221,7 +293,7 @@ while (1):
         # run the WBC to solve for the new joint configurations
         FL_leg, FR_leg, RL_leg, RR_leg, grip = LeggedRobot.runWBC(base_config, target_cartesian_pos_CoM=planner_pos[0], target_cartesian_vel_CoM=planner_vel[0], target_cartesian_pos_EE=EE_target_pos, target_cartesian_vel_EE=EE_target_vel, target_cartesian_pos_trunk=Trunk_target_pos, target_cartesian_vel_trunk=Trunk_target_vel)
         #print("py:", imu_pos)
-        base_offset = np.array([p.getLinkState(LeggedRobot_bullet, 0)[4]]) - base_offset
+        base_offset = np.array([p.getLinkState(LeggedRobot_bullet, 0)[4]])
         #print("py offset:", base_offset)
         #print("pin:", LeggedRobot.robot_data.oMf[LeggedRobot.trunk_frame_index].translation.T)
 
